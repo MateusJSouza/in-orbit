e6a7725667d16167003d1f7713e531102e4a2a64
MateusJSouza <mateusjdev@gmail.com>
2024-09-15 11:48:42 -0300

    feat: implementando componente EmptyGoals
    - Criando layout para exibição quando não há metas cadastradas
    - Adicionando imagens de logo e ilustração
    - Incluindo mensagem de incentivo para cadastrar nova meta
    - Implementando botão para abrir diálogo de cadastro de meta

--- web/src/components/empty-goals.tsx
+++ web/src/components/empty-goals.tsx
@@ -2,27 +2,30 @@ import logoInOrbit from '../assets/logo-in-orbit.svg'
 import letsStartIllustration from '../assets/lets-start-illustration.svg'
 import { Plus } from 'lucide-react'
 import { Button } from './ui/button'
+import { DialogTrigger } from './ui/dialog'
 
 export function EmptyGoals() {
   return (
-    <>
+    <main className="h-screen flex flex-col items-center justify-center gap-8">
       <img src={logoInOrbit} alt="Logo In Orbit" className="mb-8 w-32" />
       <img
         src={letsStartIllustration}
         alt="Let's Start Illustration"
         className="mb-8 w-64"
       />
-      <p className="text-center text-zinc-300 mb-8 max-w-80 leading-relaxed">
+      <p className="text-center text-zinc-300 mb-8 max-w-[400px] leading-relaxed">
         Você ainda não cadastrou nenhuma meta, que tal{' '}
         <a href="/create-goal" className="underline">
           cadastrar uma
         </a>{' '}
         agora mesmo?
       </p>
-      <Button>
-        <Plus className="size-4" />
-        Cadastrar meta
-      </Button>
-    </>
+      <DialogTrigger asChild>
+        <Button>
+          <Plus className="size-4" />
+          Cadastrar meta
+        </Button>
+      </DialogTrigger>
+    </main>
   )
 }
